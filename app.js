0d2ef86ed3dfb558e96cdf9887817de1cc072a40
Sarah Rehan <sarahahmad17@gmail.com>
2023-06-10 23:37:22 -0400

    added routes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/subscribers");
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
-db.once("open", () => console.log("Connected to db"));
 db.on("error", (err) => console.error(err));
+db.once("open", () => console.log("Connected to db"));
 
+app.use(express.json());
+
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
 app.listen(3000, () => console.log("server has started"));
